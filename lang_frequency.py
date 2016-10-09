0fb113aec64346e2e37ec3c8111ad607c68c1d40
romanyakovlev <yakovlevroman8@gmail.com>
2016-10-10 01:55:37 +0700

    исправление состваления списка слов

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,18 +1,22 @@
-
+import argparse
+from collections import Counter
+import re
 
 def load_data(filepath):
     file = open(filepath, 'r')
     return file.read()
 
 def get_most_frequent_words(text):
-    text = ' '.join(text.split('\n')).split(' ')
-    arr = sorted([x for x in list(set(text)) if x != ''])
-    mx = max(text.count(x) for x in arr)
-    for x in arr:
-        if text.count(x) == mx:
-            return x
-            break
+    text = re.findall(r'(\w+)', text.lower())
+    most_common_words_tuple = Counter(text).most_common(10)
+    return [word for word,quantity in most_common_words_tuple]
 
 if __name__ == '__main__':
-    text = load_data('1.txt')
-    print(get_most_frequent_words(text))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_name')
+    file_name = parser.parse_args().file_name
+    print('Ваш файл c данными: {}'.format(file_name))
+    text_data = load_data(file_name)
+    print('Десять самых часто встречающихся слов в тексте {}:'.format(file_name))
+    for number,word in enumerate(get_most_frequent_words(text_data)):
+        print('{} - {}'.format(number+1,word))
